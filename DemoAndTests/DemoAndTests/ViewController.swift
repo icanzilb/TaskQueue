6652a805e9dd2b045b3567e03e398722a4ef598b
Marin Todorov <marin@underplot.com>
2014-06-21 11:15:26 +0200

    initial commit

---
+++ DemoAndTests/DemoAndTests/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  TaskQueue
+//
+//  Created by Marin Todorov on 6/19/14.
+//  Copyright (c) 2014 Underplot ltd. All rights reserved.
+//
+
+import UIKit
+
+//
+// util function to delay code exection by given interval
+//
+func delay(#seconds:Double, completion:()->()) {
+    let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+    
+    dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+        completion()
+    }
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var text:UITextView
+    
+    func logToTextView(line:String) {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.text.text = self.text.text! + "\n" + line
+            })
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let queue = TaskQueue()
+        
+        queue.tasks += {
+            self.logToTextView("====== tasks ======")
+            self.logToTextView("task #1: run")
+        }
+        
+        queue.tasks += { result, next in
+            self.logToTextView("task #2: begin")
+            
+            delay(seconds: 2) {
+                self.logToTextView("task #2: end")
+                next(nil)
+            }
+            
+        }
+        
+        var cnt = 1
+        queue.tasks += {[weak queue] result, next in
+            self.logToTextView("task #3: try #\(cnt)")
+            
+            if ++cnt > 3 {
+                next(nil)
+            } else {
+                queue!.retry(delay: 1)
+            }
+        }
+        
+        queue.tasks += ({
+            self.logToTextView("task #4: run")
+            self.logToTextView("task #4: will skip next task")
+            
+            queue.skip()
+            })
+        
+        queue.tasks += {
+            self.logToTextView("task #5: run")
+        }
+        
+        queue.tasks += {
+            self.logToTextView("task #6: run")
+            
+            self.logToTextView("task #6: will append one more completion")
+            queue.run {
+                _ in self.logToTextView("completion: appended completion run")
+            }
+            
+            self.logToTextView("task #6: will skip all remaining tasks")
+            queue.removeAll()
+        }
+        
+        queue.tasks += {
+            self.logToTextView("task #7: run")
+        }
+        
+        queue.run()
+        
+        queue.run {result in
+            self.logToTextView("====== completions ======")
+            self.logToTextView("initial completion: run")
+        }
+        
+        delay(seconds: 1.5) {[weak queue] in
+            self.logToTextView("global: will pause the queue...")
+            queue!.pause()
+        }
+        
+        delay(seconds: 5) {[weak queue] in
+            self.logToTextView("global: resume the queue")
+            queue!.run()
+        }
+        
+    }
+}
+
---
+++ TaskQueue/TaskQueue.swift
@@ -0,0 +1,175 @@
+//
+// TaskQueue.swift
+//
+// Copyright (c) 2014 Marin Todorov, Underplot ltd.
+// This code is distributed under the terms and conditions of the MIT license.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// This class was heavily inspired by Sequencer (objc) https://github.com/berzniz/Sequencer
+// but aimed to 1) bring more flow control 2) port to swift
+
+import Foundation
+
+class TaskQueue {
+    
+    //
+    // types used by the TaskQueue
+    //
+    typealias ClosureNoResultNext = () -> Void
+    typealias ClosureWithResult = (AnyObject?) -> Void
+    typealias ClosureWithResultNext = (AnyObject? , AnyObject? -> Void) -> Void
+    
+    //
+    // tasks and completions storage
+    //
+    var tasks:ClosureWithResultNext[] = []
+    @lazy var completions: ClosureWithResult[] = []
+
+    //
+    // queue state
+    //
+    var running = false
+    var paused  = false
+    var stopped = false
+    
+    var currentTask: ClosureWithResultNext? = nil
+    var currentResult: AnyObject! = nil
+    
+    var hasCompletions = false
+    var delayUntilNextTask:Double = 0
+
+    //
+    // start or resume the queue
+    //
+    func run(completion:ClosureWithResult? = nil) {
+        if completion {
+            hasCompletions = true
+            completions += completion!
+        }
+        
+        if (paused) {
+            paused = false
+            _runNextTask()
+            return
+        }
+        
+        if running {
+            return
+        }
+        
+        running = true
+        _runNextTask()
+    }
+    
+    //
+    // pause the queue execution
+    //
+    func pause() {
+        paused = true
+    }
+    
+    //
+    // remove all tasks and completions
+    //
+    func stop () {
+        stopped = true
+    }
+    
+    func _runNextTask(result: AnyObject? = nil) {
+        if (stopped) {
+            tasks.removeAll(keepCapacity: false)
+            completions.removeAll(keepCapacity: false)
+        }
+        
+        currentResult = result
+        
+        if paused {
+            return
+        }
+        
+        if tasks.count == 0 {
+            _complete()
+            return
+        }
+        
+        if delayUntilNextTask > 0 {
+            
+            let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * delayUntilNextTask ))
+            
+            dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
+                self._runNextTask(result: result)
+            }
+            
+            delayUntilNextTask = 0
+            return
+        }
+        
+        currentTask = tasks.removeAtIndex(0)
+        currentTask!(result) {[weak self] (nextResult:AnyObject?) in
+            self!._runNextTask(result: nextResult)
+        }
+        
+    }
+    
+    func _complete() {
+        running = false
+        paused = false
+        currentTask = nil
+        
+        if hasCompletions {
+            for _ in completions {
+                (completions.removeAtIndex(0))(currentResult)
+            }
+        }
+        
+        currentResult = nil
+    }
+    
+    //
+    // skip the next task
+    //
+    func skip() {
+        if tasks.count>0 {
+            _ = tasks.removeAtIndex(0) //better way?
+        }
+    }
+    
+    //
+    // remove all remaining tasks
+    //
+    func removeAll() {
+        tasks.removeAll(keepCapacity: false)
+    }
+    
+    //
+    // count of the tasks left to execute
+    //
+    var count:Int {
+        return tasks.count
+    }
+    
+    //
+    // re-run the current task
+    //
+    func retry(delay:Double = 0) {
+        tasks.insert(currentTask!, atIndex: 0)
+        currentTask = nil
+        
+        if delay > 0 {
+            delayUntilNextTask = delay
+        }
+        
+        _runNextTask(result: currentResult)
+    }
+}
+
+@assignment func += (inout tasks: TaskQueue.ClosureWithResultNext[], task: TaskQueue.ClosureNoResultNext) {
+    tasks += {
+        _, next in
+        task()
+        next(nil)
+    }
+}
